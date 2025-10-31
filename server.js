ea087e478b76e1a400363dc635ca53cf68e640ee
Your Name <you@example.com>
2025-10-31 17:17:56 +0300

    feat: edit session config

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24,
     },
   })
